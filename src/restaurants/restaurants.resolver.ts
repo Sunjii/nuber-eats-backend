ed41ed8854d358d2f6991aaa8ab7554a61d27a04
Sunji <sophieadenn@gmail.com>
2021-08-06 15:57:44 +0900

    Argument 설정

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entitiy';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurnat(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
