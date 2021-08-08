b67470560cc2b66a18b08867cdd8461e71a8fc49
Sunji <sophieadenn@gmail.com>
2021-08-09 02:56:30 +0900

    update mutation

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dtos';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurants.entitiy';
 
 @Injectable()
@@ -19,5 +20,8 @@ export class RestaurantService {
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     //const newRestaurant = this.restaurants.create();
     return this.restaurants.save(newRestaurant);
+  }
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    return this.restaurants.update(id, { ...data });
   }
 }
