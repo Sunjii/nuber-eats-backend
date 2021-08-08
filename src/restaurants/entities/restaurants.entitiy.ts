9f0a22a1e8a75d7b1a4f3987af8a1d71c3ce7716
Sunji <sophieadenn@gmail.com>
2021-08-08 17:54:45 +0900

    typeORM 설정

--- src/restaurants/entities/restaurants.entitiy.ts
+++ src/restaurants/entities/restaurants.entitiy.ts
@@ -1,16 +1,30 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
 
   @Field((tpye) => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   adress: string;
 
   @Field((type) => String)
+  @Column()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  catergoryName: string;
 }
