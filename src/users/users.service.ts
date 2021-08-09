1ba6891de6e31f40a6200cab3e441eb367758f99
Sunji <sophieadenn@gmail.com>
2021-08-10 03:31:19 +0900

    Error handle

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,23 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check new user
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make error
+        return 'There is a user with that email already';
+      }
+      // create user account & hash the password
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (e) {
+      return "Couldn't create account";
+    }
+  }
 }
