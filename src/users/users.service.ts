3871a01c50f656f75f6dbfcca8c1f828f1d613eb
Sunji <sophieadenn@gmail.com>
2021-08-11 03:12:52 +0900

    Token

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,12 +4,17 @@ import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
-  ) {}
+    private readonly config: ConfigService,
+  ) {
+    //console.log(this.config.get('SECRET_KEY'));
+  }
 
   async createAccount({
     email,
@@ -52,6 +57,7 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return {
         ok: true,
         token: 'login 성공!',
