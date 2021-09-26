851a24873b18a980aa197fc9768c93aa53c558de
Sunji <sophieadenn@gmail.com>
2021-09-26 23:04:10 +0900

    JWT verify unit test done.

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -3,12 +3,14 @@ import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 import * as jwt from 'jsonwebtoken';
 
+const TEST_KEY = 'testKey';
+const USER_ID = 1;
 jest.mock('jsonwebtoken', () => {
   return {
     sign: jest.fn(() => 'TOKEN'),
+    verify: jest.fn(() => ({ id: USER_ID })),
   };
 });
-const TEST_KEY = 'testKey';
 
 describe('JwtService', () => {
   let service: JwtService;
@@ -33,15 +35,20 @@ describe('JwtService', () => {
 
   describe('sign', () => {
     it('should return a signed token', () => {
-      const ID = 1;
-      const token = service.sign(ID);
+      const token = service.sign(USER_ID);
       expect(typeof token).toBe('string');
       expect(jwt.sign).toHaveBeenCalledTimes(1);
-      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: USER_ID }, TEST_KEY);
     });
   });
 
   describe('verify', () => {
-    it('should return the decoded toekn', () => {});
+    it('should return the decoded toekn', () => {
+      const TOKEN = 'TOKEN';
+      const decodedToken = service.verify(TOKEN);
+      expect(decodedToken).toEqual({ id: USER_ID });
+      expect(jwt.verify).toHaveBeenCalledTimes(1);
+      expect(jwt.verify).toHaveBeenCalledWith(TOKEN, TEST_KEY);
+    });
   });
 });
