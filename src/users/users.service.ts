6ccbe35f2694a4bb7780dc47756e64aadd93d541
Sunji <sophieadenn@gmail.com>
2021-10-04 21:20:07 +0900

    editProfile minor bug fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -113,6 +113,9 @@ export class UsersService {
       if (email) {
         user.email = email;
         user.verified = false;
+        // delete verification when edit profile
+        await this.verifications.delete({ user: { id: user.id } });
+
         const verification = await this.verifications.save(
           this.verifications.create({ user }),
         );
