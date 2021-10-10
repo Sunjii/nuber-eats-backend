1aecbaf80502e303072e27b3625761a382299e7e
Sunji <sophieadenn@gmail.com>
2021-10-10 17:00:27 +0900

    AuthGaurd setting for userRole

--- src/auth/atuh.guard.ts
+++ src/auth/atuh.guard.ts
@@ -1,15 +1,33 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { resolveSoa } from 'dns';
+import { User } from 'src/users/entities/user.entity';
+import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext) {
     // if false, block the request
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!roles) {
+      // doesn't have a metadata
+      return true;
+    }
+
     const gglContext = GqlExecutionContext.create(context).getContext();
-    const user = gglContext['user'];
+    const user: User = gglContext['user'];
     if (!user) {
       return false;
     }
-    return true;
+    if (roles.includes('Any')) {
+      return true;
+    }
+
+    return roles.includes(user.role);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './atuh.guard';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+})
 export class AuthModule {}
