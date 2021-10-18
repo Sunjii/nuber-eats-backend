cdf18b87fbb1494a9a7bd1e01444bda6d521ec74
Sunji <sophieadenn@gmail.com>
2021-10-18 19:58:17 +0900

    Dish entity and setting

---
+++ src/restaurants/entities/dish.entity.ts
@@ -0,0 +1,42 @@
+import { InputType, ObjectType, Field, Int } from '@nestjs/graphql';
+import { IsNumber, IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Entity, Column, ManyToOne, RelationId } from 'typeorm';
+import { Restaurant } from './restaurants.entitiy';
+
+@InputType('DishInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Dish extends CoreEntity {
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field((type) => Int)
+  @Column()
+  @IsNumber()
+  price: number;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  photo: string;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(10, 140)
+  description: string;
+
+  // Many to One Relation
+  @Field((type) => Restaurant)
+  @ManyToOne((type) => Restaurant, (restaurant) => restaurant.menu, {
+    onDelete: 'CASCADE',
+  })
+  restaurant: Restaurant;
+
+  @RelationId((dish: Dish) => dish.restaurant)
+  restaurantId: number;
+}
