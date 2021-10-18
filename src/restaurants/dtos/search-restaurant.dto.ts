43078d5962a479733af83e646e1b4f558af7e4b1
Sunji <sophieadenn@gmail.com>
2021-10-18 17:45:34 +0900

    Searching 기능 완료

--- src/restaurants/dtos/search-restaurant.dto.ts
+++ src/restaurants/dtos/search-restaurant.dto.ts
@@ -14,5 +14,6 @@ export class SearchRestaurantInput extends PaginationInput {
 @ObjectType()
 export class SearchRestaurantOutput extends PaginationOutput {
   @Field((type) => [Restaurant], { nullable: true })
-  restaurants?: Restaurant[];
+  //restaurants?: Restaurant[];
+  searchingResults?: Restaurant[];
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
-import { Like, Repository } from 'typeorm';
+import { ILike, Like, Repository } from 'typeorm';
 import { AllCategoriesOutput } from './dtos/all-categories.dto';
 import { CategoryInput, CategoryOutput } from './dtos/category.dto';
 import {
@@ -248,9 +248,18 @@ export class RestaurantService {
     try {
       const [restaurnats, totalResults] = await this.restaurants.findAndCount({
         where: {
-          name: Like(`%${query}%`),
-        }, // typeORM - like
+          name: ILike(`%${query}%`),
+        }, // typeORM - ILike : find all upper and lower case
+        // todo : make pagination's repository?
+        skip: (page - 1) * 25,
+        take: 25,
       });
+      return {
+        ok: true,
+        searchingResults: restaurnats,
+        totalResults,
+        totalPages: Math.ceil(totalResults / 25),
+      };
     } catch {
       return {
         ok: false,
