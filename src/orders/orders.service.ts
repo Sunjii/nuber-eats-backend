0b2e0a38a63be9023a99ae08b17ed8ada446b964
Sunji <sophieadenn@gmail.com>
2021-10-23 03:47:25 +0900

    Edit Order 추가

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,9 +5,11 @@ import { Restaurant } from 'src/restaurants/entities/restaurants.entitiy';
 import { User, UserRole } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
+import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { OrderItem } from './entities/order-item.entity';
-import { Order } from './entities/order.entity';
+import { Order, OrderStatus } from './entities/order.entity';
 
 @Injectable()
 export class OrderService {
@@ -148,7 +150,7 @@ export class OrderService {
         // get order from restaurants
         orders = restaurants.map((restaurant) => restaurant.orders).flat(1);
         if (status) {
-          orders = orders.map((order) => order.status === status);
+          orders = orders.filter((order) => order.status === status);
         }
       }
       // return orders
@@ -161,6 +163,128 @@ export class OrderService {
       return {
         ok: false,
         error: 'Could not get orders',
+      };
+    }
+  }
+
+  // can i see that order?
+  canSeeOrder(user: User, order: Order): boolean {
+    // check user role
+    let allowed = true;
+    if (user.role === UserRole.Client && order.customerId !== user.id) {
+      allowed = false;
+    }
+    if (user.role === UserRole.Delivery && order.driverId !== user.id) {
+      allowed = false;
+    }
+    if (user.role === UserRole.Owner && order.restaurant.ownerId !== user.id) {
+      allowed = false;
+    }
+    return allowed;
+  }
+
+  // get an Order
+  async getOrder(
+    user: User,
+    { id: orderId }: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    try {
+      // find order
+      const order = await this.orders.findOne(orderId, {
+        relations: ['restaurant'],
+      });
+      if (!order) {
+        return {
+          ok: false,
+          error: 'Order not found',
+        };
+      }
+
+      if (!this.canSeeOrder(user, order)) {
+        return {
+          ok: false,
+          error: 'You cant see that',
+        };
+      }
+
+      return {
+        ok: true,
+        order,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not load order',
+      };
+    }
+  }
+
+  // edit order
+  async editOrder(
+    user: User,
+    { id: orderId, status }: EditOrderInput,
+  ): Promise<EditOrderOutput> {
+    try {
+      // get order
+      const order = await this.orders.findOne(orderId, {
+        relations: ['restaurant'],
+      });
+      if (!order) {
+        return {
+          ok: false,
+          error: 'Order not found',
+        };
+      }
+
+      // check the role
+      if (!this.canSeeOrder(user, order)) {
+        return {
+          ok: false,
+          error: "You can't see that",
+        };
+      }
+
+      let canEdit = true;
+
+      // customer can't change order status
+      if (user.role === UserRole.Client) {
+        canEdit = false;
+      }
+      // Owner와 Delivery 역시 이 상태의 주문은 수정할 수 없다.
+      if (user.role === UserRole.Owner) {
+        if (status !== OrderStatus.Cooking && status !== OrderStatus.Cooked) {
+          canEdit = false;
+        }
+      }
+      if (user.role === UserRole.Delivery) {
+        if (
+          status !== OrderStatus.PickedUp &&
+          status !== OrderStatus.Delivered
+        ) {
+          canEdit = false;
+        }
+      }
+      //
+      if (!canEdit) {
+        return {
+          ok: false,
+          error: "You can't edit order",
+        };
+      }
+      // done
+      await this.orders.save([
+        {
+          id: orderId,
+          status,
+        },
+      ]);
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: "Can't edit order",
       };
     }
   }
