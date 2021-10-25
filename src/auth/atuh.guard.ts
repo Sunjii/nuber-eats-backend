32c64efb24a2e6c1bf03257517009201cee49d17
Sunji <sophieadenn@gmail.com>
2021-10-25 14:50:49 +0900

    Authguard의 subscription 적용을 위한 수정

--- src/auth/atuh.guard.ts
+++ src/auth/atuh.guard.ts
@@ -2,13 +2,19 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { resolveSoa } from 'dns';
+import { JwtService } from 'src/jwt/jwt.service';
 import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     // if false, block the request
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
@@ -19,21 +25,36 @@ export class AuthGuard implements CanActivate {
       return true;
     }
 
-    const gqlContext = GqlExecutionContext.create(context).getContext();
     // console.log(gqlContext.token);
     // WS --> undefined
     // 누가 gaurad에게 context를 주었을까?
     // HTTP request -> jwt-middle ware -> take token from header, and find user
     // jwt put in req -> gql find user
-    const user: User = gqlContext['user'];
-    if (!user) {
-      // invalid token. can't go ahead...
+
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const token = gqlContext.token;
+
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        // attach the user to request object
+        if (!user) {
+          // invalid token. can't go ahead...
+          return false;
+        }
+        // guard가 user를 gqlcontext에 추가
+        gqlContext['user'] = user;
+        if (roles.includes('Any')) {
+          return true;
+        }
+        // done
+        return roles.includes(user.role);
+      } else {
+        return false;
+      }
+    } else {
       return false;
     }
-    if (roles.includes('Any')) {
-      return true;
-    }
-
-    return roles.includes(user.role);
   }
 }
